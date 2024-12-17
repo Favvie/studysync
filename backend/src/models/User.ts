f314aa566a340ee7b475f118558cb80785162cff
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-12-17 22:57:21 +0000

    Add User schema

---
+++ backend/src/models/User.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+    }
+}, { timestamps: true });
+
+export const User = mongoose.model("Users", UserSchema);
